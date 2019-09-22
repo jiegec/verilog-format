79b42298b08dd528891f5b79fd13d5e227198e90
Runji Wang <wangrunji0408@163.com>
2019-09-22 15:42:29 +0800

    rewrite lexer using macro

--- src/test.rs
+++ src/test.rs
@@ -7,15 +7,13 @@ use pretty_assertions::assert_eq;
 
 #[test]
 fn test_files() {
-    for (orig, fmt) in &[
-        ("./tests/Encoder.sv", "./tests/Encoder.fmt.sv"),
-        ("./tests/comment.sv", "./tests/comment.fmt.sv"),
-        ("./tests/test_cpu.sv", "./tests/test_cpu.fmt.sv"),
-    ] {
-        let text = read_to_string(orig).unwrap();
+    for testcase in &["Encoder", "comment", "test_cpu"] {
+        let input_path = format!("./tests/{}.sv", testcase);
+        let expected_path = format!("./tests/{}.fmt.sv", testcase);
+        let text = read_to_string(input_path).unwrap();
         let result = lexer::tokens(&text).unwrap();
         let printed_text = printer::printer(result.1).unwrap();
-        let expected_text = read_to_string(fmt).unwrap();
+        let expected_text = read_to_string(expected_path).unwrap();
         if printed_text != expected_text {
             println!("Expected:\n{}", expected_text);
             println!("Actual:\n{}", printed_text);
